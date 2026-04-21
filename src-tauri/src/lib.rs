a0bbdf3b2ceb12c6e58d6be50edb15d9dc26badb
Hattori-Arch <hattori.index.js@asagi.waseda.jp>
2026-04-21 17:58:40 +0900

    spectaによる型生成、rusqliteによるsqliteデータベースのセットアップ

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,90 @@
+#[cfg(debug_assertions)]
+use rusqlite::{params, Connection};
+use serde::{Deserialize, Serialize};
+use specta::Type;
+use specta_typescript::Typescript;
+use std::sync::Mutex;
+use tauri::Manager;
+use tauri_specta::{collect_commands, Builder};
+
+#[derive(Serialize, Deserialize, Type)]
+pub struct ShoppingItem {
+	pub id: i32,
+	pub name: String,
+	pub quantity: i32,
+}
+
+pub struct DBState {
+	pub conn: Mutex<Connection>,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
 	format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+#[specta::specta]
+fn add_item(state: tauri::State<DBState>, name: String, quantity: i32) -> Result<(), String> {
+	let conn = state.conn.lock().map_err(|e| e.to_string())?;
+
+	conn.execute("INSERT INTO shopping_items (name, quantity) VALUES (?1, ?2)", params![&name, &quantity]).map_err(|e| e.to_string())?;
+	Ok(())
+}
+
+#[tauri::command]
+#[specta::specta]
+fn get_items(state: tauri::State<DBState>) -> Result<Vec<ShoppingItem>, String> {
+	let conn = state.conn.lock().map_err(|e| e.to_string())?;
+	let mut stmt = conn.prepare("SELECT id, name, quantity FROM shopping_items").map_err(|e| e.to_string())?;
+	let items_iter = stmt
+		.query_map([], |row| {
+			Ok(ShoppingItem {
+				id: row.get(0)?,
+				name: row.get(1)?,
+				quantity: row.get(2)?,
+			})
+		})
+		.map_err(|e| e.to_string())?;
+	let items = items_iter.collect::<Result<Vec<_>, _>>().map_err(|e| e.to_string())?;
+	Ok(items)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+	let specta_builder = Builder::<tauri::Wry>::new().commands(collect_commands![greet, add_item, get_items]);
+
+	#[cfg(debug_assertions)]
+	specta_builder.export(Typescript::default(), "../src/types/bindings.ts").expect("Failed to export TypeScript bindings");
+
 	tauri::Builder::default()
+		.setup(|app| {
+			// OS固有パス
+			let handle = app.handle();
+			let app_dir = handle.path().app_data_dir().expect("Failed to get app dir");
+
+			// フォルダがなければ作成
+			std::fs::create_dir_all(&app_dir).unwrap();
+			let db_path = app_dir.join("hitorigurashi.db");
+
+			// 接続、テーブル作成
+			let conn = Connection::open(&db_path).unwrap();
+			conn
+				.execute(
+					"CREATE TABLE IF NOT EXISTS shopping_items (id INTEGER PRIMARY KEY, name TEXT NOT NULL, quantity INTEGER)",
+					[],
+				)
+				.unwrap();
+
+			// Tauriの状態に接続を保存
+			app.manage(DBState { conn: Mutex::new(conn) });
+
+			Ok(())
+		})
+		.invoke_handler(specta_builder.invoke_handler())
 		.plugin(tauri_plugin_opener::init())
-		.invoke_handler(tauri::generate_handler![greet])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -6,9 +6,23 @@ import {
 } from "@/components/layout"
 import { useRouter } from "@/hooks/useRouter"
 import { createRoute } from "@/router"
+import { commands, ShoppingItem } from "@/types/bindings"
+import { useState } from "react"
 
 export const HomeRoute = createRoute("home", () => {
 	const { navigate, currentRoute } = useRouter()
+	const [itemName, setItemName] = useState<ShoppingItem["name"]>("")
+	const [items, setItems] = useState<ShoppingItem[]>([])
+
+	const handleAddItem = async () => {
+		await commands.addItem(itemName, 1)
+		setItemName("")
+		const res = await commands.getItems()
+		if (res.status === "ok") {
+			setItems(res.data)
+		}
+	}
+
 	return (
 		<Layout>
 			<LayoutHeader>Home</LayoutHeader>
@@ -19,6 +33,19 @@ export const HomeRoute = createRoute("home", () => {
 					value="Go to About"
 					onClick={() => navigate("about")}
 				/>
+				<input
+					type="text"
+					value={itemName}
+					onChange={(e) => setItemName(e.target.value)}
+				/>
+				<input type="button" onClick={handleAddItem} value="Add Item" />
+				<ul>
+					{items.map((item) => (
+						<li key={item.id}>
+							{item.name} x {item.quantity}
+						</li>
+					))}
+				</ul>
 			</LayoutBody>
 			<LayoutNavbar>
 				<div>test</div>
